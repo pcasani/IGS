7543a79071b5049b3f969aa6ad343c0c7b3415a1
Paolo Casani <49405745+pcasani@users.noreply.github.com>
2021-05-11 09:15:11 +0000

    Update organisations.controller.js

--- src/api/organisations/controller/organisations.controller.js
+++ src/api/organisations/controller/organisations.controller.js
@@ -5,9 +5,6 @@
 const request = require('request');
 const {Buffer} = require('safe-buffer');
 
-// Used for SSL
-// const fs = require("fs");
-
 const organisationAddress = process.env.NETWORK_STATUS;
 const microServicesAuthorizationKey = process.env.MS_AUTHORIZATION_KEY;
 const microServicesAuthorizationValue = process.env.MS_AUTHORIZATION_VALUE;
@@ -38,32 +35,31 @@ exports.getOrganisationId = function (req, res) {
         });
 };
 
-// 20190913: temporarily commented out until implementation is clear
-// Adds SSL
-// exports.getNetworkDeviceMetrics = function (req, res) {
-//
-//     const {orgId, deviceId} = req.params;
-//
-//     request.get(
-//         {
-//             request.get(`${organisationAddress}/api/v1/orgs/${orgName}`, {json: true})
-//             agentOptions: {
-//                 key: fs.readFileSync("../../../../config/env/key.pem"),
-//                 cert: fs.readFileSync("../../../../config/env/chain.pem")
-//             },
-//             json: true
-//         }
-//     )
-//         .headers(apiKeyCode)
-//         .on("response", function (response) {
-//             console.log(response.statusCode);
-//             res.send({
-//                 success: "true",
-//                 message: "Network device groups data retrieve successfully",
-//                 health: response
-//             });
-//         })
-//         .on("error", function () {
-//             res.statusCode();
-//         });
-// };
+
+exports.getNetworkDeviceMetrics = function (req, res) {
+
+     const {orgId, deviceId} = req.params;
+
+     request.get(
+         {
+             request.get(`${organisationAddress}/api/v1/orgs/${orgName}`, {json: true})
+             agentOptions: {
+                 key: fs.readFileSync("../../../../config/env/key.pem"),
+                 cert: fs.readFileSync("../../../../config/env/chain.pem")
+             },
+             json: true
+         }
+     )
+         .headers(apiKeyCode)
+         .on("response", function (response) {
+             console.log(response.statusCode);
+             res.send({
+                 success: "true",
+                 message: "Network device groups data retrieve successfully",
+                 health: response
+             });
+         })
+         .on("error", function () {
+             res.statusCode();
+         });
+};
