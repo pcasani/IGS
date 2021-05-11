ef68e08b0e87b9da6ed8857e4c0eefd5d6f1b070
Paolo Casani <49405745+pcasani@users.noreply.github.com>
2021-05-11 09:27:38 +0000

    Update test.routes.config.js

--- config/routes/test.routes.config.js
+++ config/routes/test.routes.config.js
@@ -2,10 +2,10 @@
 /* jslint es6 */
 
 const NetworkHealth = require("../../src/api/nms/controllers/nms.controller");
-//const Organizations = require("../../src/api/versa-service/controllers/versa.service.controller");
+const Organizations = require("../../src/api/versa-service/controllers/versa.service.controller");
 
-//const VerityUser = require("../../src/middleware/validation/auth.token.ums.js");
-//const InitializeToken = require("../../src/middleware/authentication/auth.initialize.token.js");
+const VerityUser = require("../../src/middleware/validation/auth.token.ums.js");
+const InitializeToken = require("../../src/middleware/authentication/auth.initialize.token.js");
 
 exports.routesConfig = function (app) {
     /**
@@ -18,50 +18,38 @@ exports.routesConfig = function (app) {
     /**
      * Retrieves organisations from Versa Server
      */
-/*
-
     app.get("/api/v1/organisations/open", [
         Organizations.retrieveOrganizationSingle
     ]);
-*/
 
-/**
+    /**
      * Retrieves organisations from Versa Server (without token)
      */
-/*
     app.get("/api/v1/organisations/open", [
         Organizations.retrieveOrganization
     ]);
-*/
-
 
     /**
      * Retrieves organisations from Versa Server with token
      */
-/*
     app.get("/api/v1/organisations", [
         AuthTokenUMS.displayJWT,
         Organizations.retrieveOrganizations
     ]);
-*/
 
     /**
      * Retrieves network models version with token
-     * TODO
      */
-/*
     app.get("/api/v1/networkstatus/models/version", [
         AuthTokenUMS.displayJWT,
         NetworkHealth.getModelVersion
     ]);
-*/
+
     /**
      * Retrieves network status with token
      */
-/*
     app.get("/api/v1/networkstatus/healthTest", [
         AuthTokenUMS.displayJWT,
         NetworkHealth.getNetworkStatus
     ]);
-*/
 };
